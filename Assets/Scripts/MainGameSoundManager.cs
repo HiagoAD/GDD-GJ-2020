37c52858dfb048bb57324198321b8419e79b1249
Hiago <hiago@manifestohut.com.br>
2020-11-01 13:51:22 -0300

    Added BG sound and fixed some buildings in game scene

---
+++ Assets/Scripts/MainGameSoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainGameSoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource startClip;
+    [SerializeField] AudioSource loopClip;
+
+    AudioSource source;
+    private void Start()
+    {
+        startClip.Play();
+        StartCoroutine(PlayLoop());
+    }
+
+    IEnumerator PlayLoop()
+    {
+        yield return new WaitForSeconds(startClip.clip.length - 0.5f);
+        loopClip.Play();
+    }
+}
